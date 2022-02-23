04ec4df08b3347e63794c01f9f6f0af4f4860760
Scott Bailey <sbailey@anaconda.com>
2022-02-23 10:46:14 -0700

    Initial merge and update of conda-forge recipe.

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -1,6 +1,7 @@
 mkdir build-cpp
 cd build-cpp
-del CMakeCache.txt
+if errorlevel 1 exit 1
+
 
 IF not x%PKG_NAME:static=%==x%PKG_NAME% (
     set BUILD_TYPE="-DBUILD_SHARED_LIBS=OFF"
@@ -8,19 +9,22 @@ IF not x%PKG_NAME:static=%==x%PKG_NAME% (
     set BUILD_TYPE="-DBUILD_SHARED_LIBS=ON"
 )
 
-cmake .. ^
-      -G "NMake Makefiles" ^
-      -DCMAKE_BUILD_TYPE=Release ^
+cmake -G "Ninja" ^
+      %CMAKE_ARGS% ^
+      %BUILD_TYPE% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DREPROC_TEST=ON ^
       -DREPROC++=ON ^
-      %BUILD_TYPE%
+      %SRC_DIR%
 
-nmake all
-nmake test
-nmake install
+ninja
+ninja test
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
 
 IF not x%PKG_NAME:static=%==x%PKG_NAME% (
     REN %LIBRARY_PREFIX%\lib\reproc++.lib reproc++_static.lib
